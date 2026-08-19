ca7f9d6f4bce3d2234996d9a40e79642faf02956
camlo <camilopasten.arancibia@gmail.com>
2026-08-19 18:07:35 -0400

    add: fixing the javascript and made up the conversor function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,31 @@
-async function convertirDivisa() {
+const montoInput = document.querySelector("#monto")
+const divisaSelected = document.querySelector("#divisas")
+const btnConv = document.querySelector("#btn-conversion")
+const montoConvertido = document.querySelector("#resultado-conversion")
+
+
+async function llamadoDivisas() {
     try {
         const res = await fetch("https://mindicador.cl/api/");
         const divisas = await res.json();
         return divisas
     } catch (error) {
-        alert(e.message)
+        alert(error.message)
     }
 }
+async function convertirDivisas(){
+    const monto = Number(montoInput.value);
+    const divisaElegida = divisaSelected.value;
 
+    if (monto <= 0){
+        alert('Ingresa un monto válido mayor a 0')
+        return;
+    }
+
+   const divisas = await llamadoDivisas();
+
+    let conversion = monto * divisas[divisaElegida].valor;
+    montoConvertido.textContent = `Son: ${conversion.toFixed(2)} CLP`
+}
+
+btnConv.addEventListener('click', convertirDivisas);
